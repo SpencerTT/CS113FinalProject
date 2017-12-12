eace7fda32b4b07a4f786226de29de4a219e53ac
w7122251 <sai.fai.005@gmail.com>
2017-12-12 14:00:43 -0800

    ListGraph parital commit

---
+++ MineSweeper/src/ListGraph.java
@@ -0,0 +1,25 @@
+import java.util.*;
+/*
+ * A ListGraph is a class that uses an 
+ * array of lists to represent edges
+ */
+public class ListGraph  implements Graph
+{
+	//Data Field
+	/**
+	 * An array of Lists to contain the edges 
+	 * that originate with each vertex.
+	 */
+	private List<Edge>[]edges;
+	
+	//constructor
+	public ListGraph(int numV, boolean directed)
+	{
+		super(numV, directed)
+		edges = new List[numV];
+		for(int i =0; i < numV; i++)
+		{
+			edges[i] = new LinkedList<Edge>();
+		}
+	}
+}
