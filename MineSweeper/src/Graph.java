e7f7125f6a679a3d1c1152cb8aa22b104261e706
Spencer Thompson <spencerthompson@me.com>
2017-12-12 14:01:54 -0800

    Commit without typo

--- MineSweeper/src/Graph.java
+++ MineSweeper/src/Graph.java
@@ -11,7 +11,7 @@ public interface Graph
 	
 	boolean isEdge(int source, int dest);
 	
-	E getEdge(int source, int dest);
+	Edge getEdge(int source, int dest);
 	
 	Iterator<Edge> edgeIterator(int source);
 }
