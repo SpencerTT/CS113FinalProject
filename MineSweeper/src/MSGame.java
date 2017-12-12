1bd115696ec6025c86b7b056a723d70c79a7caf4
Spencer Thompson <spencerthompson@me.com>
2017-12-12 13:13:30 -0800

    Finished Product
    User Manual in MSGame

--- MineSweeper/src/MSGame.java
+++ MineSweeper/src/MSGame.java
@@ -3,6 +3,13 @@
  *
  * @Authors: Spencer, Shawn, Eric
  * @version 1.0
+ * 
+ * User Manual:
+ * 1. Select a field length radio button
+ * 2. Select a field density radio button
+ * 3. Select the new game button
+ * Left-click is explore tile
+ * Right-click to flag or unflag a tile
  */
 public class MSGame
 {
