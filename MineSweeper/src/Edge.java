100b35a6ca59165806a860f1faaa9beb8dff288e
Eric Ackley <ackley.eric@gmail.com>
2017-12-12 13:51:25 -0800

    Edge class for listGraph https://github.com/MiraCostaCS113/TuTh_GroupC.git

---
+++ MineSweeper/src/Edge.java
@@ -0,0 +1,36 @@
+
+public class Edge {
+private int destination;
+private int source;
+private double weight;
+public Edge(int source, int destination){
+	this.source= source;
+	this.destination = 1;
+}
+public Edge(int source, int destination, double weight){
+	this.source = source;
+	this.destination = destination;
+	this.weight = weight;
+}
+public boolean equals(Object other){
+	if(other != getClass()){
+		return false;}
+	Edge temp = new Edge(other.getSource(), other.getDestination(), other.getWeight());
+	return (temp.getSource() == getSource() && temp.getDestination() == getDestination() && temp.getWeight() == getWeight());
+	}
+public int getDestination(){
+	return destination;
+}
+public int getSource(){
+	return source;
+}
+public double getWeight(){
+	return weight;
+}
+public String toString(){
+	return source + ":" + destination + " weight of " + weight;
+}
+public int hashCode(){
+	return source - destination;
+}
+}
