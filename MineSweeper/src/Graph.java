b63ce087f66272592bd94be694c536b137b58bd3
Spencer Thompson <spencerthompson@me.com>
2017-12-12 13:56:31 -0800

    Graph interface

--- MineSweeper/src/Graph.java
+++ MineSweeper/src/Graph.java
@@ -1,4 +1,17 @@
-public class Graph
+import java.util.Iterator;
+
+public interface Graph
 {
 	
+	int getNumV();
+	
+	boolean isDirected();
+	
+	void insert(Edge edge);
+	
+	boolean isEdge(int source, int dest);
+	
+	E getEdge(int source, int dest);
+	
+	Iterator<Edge> edgeIterator(int source);
 }
