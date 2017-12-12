2783553e66a536a2337a184fb2f824617f1bbdf3
w7122251 <sai.fai.005@gmail.com>
2017-12-12 14:52:30 -0800

    Comments

--- MineSweeper/src/AbstractGraph.java
+++ MineSweeper/src/AbstractGraph.java
@@ -5,43 +5,47 @@ public class AbstractGraph implements Graph
 
 	private int numV;
 	private boolean directed;
-	
+	//Constructor
 	public AbstractGraph(int numV, boolean directed)
 	{
 		this.numV = numV;
 		this.directed = directed;
 	}
-	
+	/**
+	 * The number of vertices.
+	 */
 	@Override
 	public int getNumV()
 	{
 		return numV;
 	}
-
+	/**
+	 * Returns true if the graph is a directed graph.
+	 */
 	@Override
 	public boolean isDirected()
 	{
 		return directed;
 	}
-
+	//stub
 	@Override
 	public void insert(Edge edge)
 	{
 		
 	}
-
+	//stub
 	@Override
 	public boolean isEdge(int source, int dest)
 	{
 		return false;
 	}
-
+	//stub
 	@Override
 	public Edge getEdge(int source, int dest)
 	{
 		return null;
 	}
-
+	//stub
 	@Override
 	public Iterator<Edge> edgeIterator(int source) {
 		return null;
